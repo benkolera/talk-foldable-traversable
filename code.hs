65db1f2b6fae5c9a26fab12f0fe571ca3c600801
Ben Kolera <ben.kolera@gmail.com>
2014-09-23 10:49:36 +1000

    And here it is... Done!

--- code.hs
+++ code.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE DeriveFunctor #-}
-module Main where
+module Examples where
 
 import Prelude hiding (mapM_)
+import Control.Applicative
 import Data.Foldable
 import Data.Monoid
 import Data.Traversable
+
+
 
 data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
   deriving (Show,Functor)
@@ -31,16 +34,53 @@ example2 = mapM_ print exampleTree1
 -- Prints 1 then 2 then 3 then 4 all on their own lines.
 
 -- |
--- >> example3
+-- >>> example3
 -- "abc"
 example3 = fold (Node (Leaf "a") "b" (Leaf "c"))
 
 -- |
--- >> example4
+-- >>> example4
 -- Just 2
 example4 = find even exampleTree1
 
 -- |
--- >> example5
+-- >>> example5
 -- [1,2,3,4]
 example5 = toList exampleTree1
+
+instance Traversable Tree where
+-- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+   traverse f Empty        = pure Empty
+   traverse f (Leaf x)     = Leaf <$> f x
+   traverse f (Node l k r) = Node <$> traverse f l <*> f k <*> traverse f r
+
+-- |
+-- >>> example6
+-- Node (Leaf "I'm from A") "I'm from B" Empty
+example6 = traverse readFile (Node (Leaf "fileA") "fileB" Empty)
+
+-- Node
+--  <$> traverse readFile (Leaf "fileA")
+--  <*> readFile "fileB"
+--  <*> traverse readFile Empty
+
+-- Node
+--   <$> (Leaf <$> readFile "fileA")
+--   <*> readFile "fileB"
+--   <*> pure Empty
+
+newtype Id a = Id { getId :: a } deriving (Functor)
+instance Applicative Id where
+  pure = Id
+  Id f <*> Id x = Id (f x)
+-- |
+-- >>> fmap' (*2) exampleTree1
+-- Node (Leaf 2) 4 (Node Empty 6 (Leaf 8))
+fmap' :: Traversable t => (a -> b) -> t a -> t b
+fmap' f = getId . traverse (Id . f)
+
+-- |
+-- >>> foldMap' Sum exampleTree1
+-- Sum {getSum = 10}
+foldMap' :: (Traversable t, Monoid m) => (a -> m) -> t a -> m
+foldMap' f = getConst . traverse (Const . f)
